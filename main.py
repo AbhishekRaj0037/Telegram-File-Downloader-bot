fc45112555adb55db723e29b46814ba283c85c08
Abhishek_Raj0037 <me_abhishek.tethered@icloud.com>
2024-11-19 02:56:43 +0530

    Added basic functionality and taking URL functionality to the app

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from pyrogram import filters,Client
+from pyromod import listen
+import validators
 import asyncio
-from pyrogram import Client,filters
 
 api_id = 27690446
 api_hash = "d56f7ac259b7c1dab704103ee45b0927"
@@ -8,11 +10,16 @@ bot_token = "7244121655:AAHbyHRnT2KA98TnYP1K8tvDblPICU7Gp7g"
 # app = Client("UFDBOT0037", api_id=api_id, api_hash=api_hash)
 app = Client("UFDBOT0037")
 
-@app.on_message(filters.command('start'))
-def welcome(app,message):
-    app.send_message(message.chat.id,"Welcome to UFDBot")
-    app.send_message(message.chat.id,"Please send link to upload file")
-        
-
+@app.on_message(filters.command(["start"]))
+async def my_handler(client, message):
+    chat=message.chat
+    await app.send_message("@UFD0037Bot", "UDF Bot is Alive !")
+    URL = await chat.ask('Please send URL to upload file', filters=filters.text)
+    is_url_valid=validators.url(URL.text)
+    while is_url_valid != True:
+        await app.send_message("@UFD0037Bot", "URL is invalid !")
+        URL=await chat.ask('Please send URL again to upload file', filters=filters.text)
+        is_url_valid=validators.url(URL.text)
+    
 
 app.run()
