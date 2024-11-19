41b91a8576fd17b7cd48078c7c0db9a679dec865
Abhishek_Raj0037 <me_abhishek.tethered@icloud.com>
2024-11-20 03:18:37 +0530

    Added basic code to upload files on telegram using URL with smaller file size

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ bot_token = "7244121655:AAHbyHRnT2KA98TnYP1K8tvDblPICU7Gp7g"
 # app = Client("UFDBOT0037", api_id=api_id, api_hash=api_hash)
 app = Client("UFDBOT0037")
 
+
+async def upload_file(url):
+    async def progress(current, total):
+        print(f"{current * 100 / total:.1f}%")
+    await app.send_document("@UFD0037Bot", document=url, progress=progress)
+
+
 @app.on_message(filters.command(["start"]))
 async def my_handler(client, message):
     chat=message.chat
@@ -20,6 +27,8 @@ async def my_handler(client, message):
         await app.send_message("@UFD0037Bot", "URL is invalid !")
         URL=await chat.ask('Please send URL again to upload file', filters=filters.text)
         is_url_valid=validators.url(URL.text)
+    await upload_file(URL.text)
+
     
 
 app.run()
